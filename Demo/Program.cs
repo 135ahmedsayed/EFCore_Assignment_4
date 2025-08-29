7cdb9ad1bd7cb03ea26bf01626dd6cc1d6b5030b
135ahmedsayed <ahmedshkra246@gmail.com>
2025-08-29 20:04:27 +0300

    Joins

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Demo
+﻿using Demo.Context;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            using var context = new NorthwindContext();
+            #region Joins
+            var Results = context.Employees.Join(context.Orders, e => e.EmployeeId, o => o.EmployeeId,
+                (e, o) => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    o.OrderId,
+                    o.OrderDate
+                });
+            foreach (var r in Results)
+            {
+                Console.WriteLine($"{r.FirstName} {r.LastName} {r.OrderId} {r.OrderDate}");
+            }
+            #endregion
             Console.ReadKey();
         }
     }
